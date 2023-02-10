8f405a9ea143e69f0a03548de51976efa2d6f821
Vasja Volin <vasja.volin@gmail.com>
2023-02-09 22:57:45 -0800

    finish up generating viles and then setting up phone # extraction

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express');
+const routes = require('./src/routes')
 
 const app = express();
-
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send({ foo: 'bar' });
-});
+// ensure we can parse JSON requests
+app.use(express.json())
+app.use('/v1', routes);
 
 app.listen(port, () => {
-  console.info(`app listening on ${port}`)
+  console.info(`app listening on ${port}`);
 });
 
